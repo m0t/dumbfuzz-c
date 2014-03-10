0d27e87a0559b8ff8299d0386e3af154d861aed2
m0t <m0t@users.noreply.github.com>
2014-03-10 18:43:24 +0000

    more scraps

--- dumbfuzz.py
+++ dumbfuzz.py
@@ -1,6 +1,17 @@
 #!/usr/bin/gdb -x 
+
+'''
+gdb python script
+
+notes:
+    - you should avoid running manually the same target program while this is running, get_process_pid will not like it
+'''
+
 import gdb
+import psutil
+import optparse
 import os
+import sys
 
 ####GLOBAL###
 exePath="/usr/lib/libreoffice/program/soffice.bin"
@@ -33,24 +44,60 @@ def die(msg):
 def fuzz_testcase(testcase, fuzzDst):
     global fuzzerPath
     global fuzzIter
-    if not os.path.exist(fuzzDst):
+    global debugFlag
+    if not os.path.exists(fuzzDst):
+        if debugFlag:
+            gdb.write("creating dir %s\n" % (fuzzDst))
         os.mkdir(fuzzDst)
-    ret = os.system("%s -n %d -o %s/fuzzed-%%n.ppt %s" % (fuzzerPath, fuzzIter, fuzzDst, testcase))
+    if debugFlag:
+        fuzzCmd = "%s -v -n %d -o %s/fuzzed-%%n.ppt %s" % (fuzzerPath, fuzzIter, fuzzDst, testcase)
+        gdb.write(fuzzCmd)
+    else:
+        fuzzCmd = "%s -n %d -o %s/fuzzed-%%n.ppt %s" % (fuzzerPath, fuzzIter, fuzzDst, testcase)
+    ret = os.system(fuzzCmd)
     if (ret != 0):
         die("fuzzer failed to run")
     return
+
+def empty_fuzzdir(fuzzDst):
+    os.system("rm -rf %s/*" % fuzzDst)
+
+#we should do this with gdb, idiot
+def get_process_pid(pname):
+    process = filter(lambda p: p.name == pname, psutil.process_iter())
+    #there should be one and only one, check this
+    if process = None:
+        die("no process found, this is bad")
+
+def parse_args():
+    parser = optparse.OptionParser("%prog [-d]")
+    parser.add_option("-d", "--debug", help="get debug output", action="store_true", dest="debug", default=True)  
     
+    return parser.parse_args()
 
 def main():
     global exePath
     global gdbArgs
     global testcasesPath
     global fuzzDst
-    #for each file in os.listdir(testcasesPath)
+    
+    global debugFlag
+    
+    opts, args = parse_args()
+    
+    debugFlag = opts.debug
+    
     f=os.listdir(testcasesPath)[0]
     fpfile="%s/%s" % (testcasesPath, f)
-    #gdb.execute("file %s" % exePath)
-    #gdb.execute("r %s %s" % (gdbArgs,fpfile))
+    #fuzz_testcase(fpfile, fuzzDst)
+    for each file in os.listdir(fuzzDst): 
+        gdb.execute("file %s" % exePath)
+        gdb.execute("r %s %s" % (gdbArgs,fpfile))
+    
+        pid=get_process_pid(os.path.basename(exePath) )
+        
+        
+    #empty_fuzzdir(fuzzDst)
 
-def __main__():
-    main()
+
+main()
