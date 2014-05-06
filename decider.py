53a8f879ba0126cb733dfc9624a057fd1907462c
m0t <m0t@users.noreply.github.com>
2014-05-06 18:14:14 +0100

    fix

--- decider.py
+++ decider.py
@@ -1,13 +1,14 @@
 class Decider(Object):
     quorum=1
-    save_quorum=1    
+    save_quorum=1
+    rules_file='decision_rules.py'    
     
     def __init__(self,save_arg=False):
         self.votes=0
         self.save_votes=0
         self.save_arg=save_arg
         
-    def update(mean, sigma2, timecounter):
+    def update(self, mean, sigma2, timecounter):
         #init votes to 0
         if self.votes < 0:
             self.votes = 0
@@ -15,17 +16,18 @@ class Decider(Object):
         weight=0
         
         #run decision rules, all vars should be already declared at this point
-        from decision_rules import *    
+        code=compile(open(rules_file).read(),'<string>','exec')
+        exec code
 
         self.votes += weight
         return True
         
-    def isQuorumReached():
+    def isQuorumReached(self):
         if self.votes >= self.quorum:
             return True
         return False
             
-    def isSaveQuorumReached():
+    def isSaveQuorumReached(self):
         if self.save_votes >= self.save_quorum:
             return True
         return False
