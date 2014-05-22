f66a7697ef65c0e9455414c12f8c45eb2f6bdf25
m0t <m0t@users.noreply.github.com>
2014-05-22 15:30:47 +0100

    launcher class

--- launcher.py
+++ launcher.py
@@ -16,53 +16,50 @@ from gdbsettings import *
 import gdbwrapper
 
 debugFlag = True
-logpath='logs/'
+
 #XXX, at some point, move everything in a launcher class
-GDB=None
+#GDB=None
 class Launcher(object):
     def __init__():
-        GDB = 
+        self.GDB = gdbwrapper.GDBWrapper()
+        self.logpath='logs/'
         
     #XXX not a very good strategy
     def get_inputfile(args):
         inputfile = args[0].split(" ")[-1]
-        GDB.debug_msg("input file should be %s" % inputfile)
+        self.GDB.debug_msg("input file should be %s" % inputfile)
         if os.path.isfile(inputfile):
             return inputfile
         return False
 
-    def check_logdir(logpath):
-        if not os.path.exists(logpath):
-            GDB.debug_msg("logs dir not found, creating")
+    def check_logdir():
+        if not os.path.exists(self.logpath):
+            self.GDB.debug_msg("logs dir not found, creating")
             try:
-                os.mkdir(logpath)
+                os.mkdir(self.logpath)
             except PermissionError:
-                GDB.debug_msg("log dir creation failed, dying")
+                self.GDB.debug_msg("log dir creation failed, dying")
                 sys.exit(-1)
 
     #save testcase. if we passed the whole testcases folder, detect this and copy the whole bloody folder, 
     #
     def save_testcase():
         #global GDB
-        global logpath
-    
-        if logpath
+        #global logpath
     
         strtime=time.strftime('%d-%m-%y_%H%M')
         savefile="fuzzedcase-"+strtime
         self.GDB.debug_msg('Saving testcase to ' + savefile)
         fuzzedcase = get_inputfile(args)
         if fuzzedcase:
-            shutil.copy(fuzzedcase, logpath+savefile)
+            shutil.copy(fuzzedcase, self.logpath+savefile)
         else:
-            GDB.debug_msg("fuzzed case not found?")
+            self.GDB.debug_msg("fuzzed case not found?")
     
     def run():
 
-        self.GDB = gdbwrapper.GDBWrapper()
-
-        check_logdir(logpath)
-
+        check_logdir(self.logpath)
+        GDB=self.GDB
         GDB.execute("set disassembly-flavor intel")
         GDB.execute("handle SIGSEGV stop print nopass")
     
@@ -80,14 +77,14 @@ class Launcher(object):
         if state != 'STOPPED':
             #XXX more contextual naming for saved testcase
             GDB.debug_msg('Crash detected, saving crashdump and testcase')
-            GDB.write_crashdump('fuzzlog', logpath, echo=True)
+            GDB.write_crashdump('fuzzlog', self.logpath, echo=True)
         
             strtime=time.strftime('%d-%m-%y_%H%M')
             savefile="fuzzedcase-"+strtime
             GDB.debug_msg('Saving testcase to ' + savefile)
             fuzzedcase = get_inputfile(args)
             if fuzzedcase:
-                shutil.copy(fuzzedcase, logpath+savefile)
+                shutil.copy(fuzzedcase, self.logpath+savefile)
             else:
                 GDB.debug_msg("fuzzed case not found?")
         
@@ -98,4 +95,5 @@ class Launcher(object):
             GDB.debug_msg("Process terminated normally")
             sys.exit(0)
 
-main()
+launcher=Launcher()
+launcher.run()
