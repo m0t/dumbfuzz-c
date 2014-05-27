02dd0898ff90a7b880b8acd8b2799c91399beb76
m0t <m0t@users.noreply.github.com>
2014-05-27 18:48:04 +0100

    fix

--- dumbfuzz-c.py
+++ dumbfuzz-c.py
@@ -173,7 +173,7 @@ def main():
             start=int(opts.skipto)
         for i in range(start,len(filelist)):
             f = filelist[i]
-            if not opts.nofuzz or not opts.runonly:
+            if not opts.nofuzz and not opts.runonly:
                 empty_fuzzdir(fuzzDst)
                 debug_msg('fuzzing testcase #%d : %s' % (i,f))
                 fuzz_testcase(f, fuzzDst)
