4745ed298b274f06f40ef558a8fc82bcc7a0b0a9
m0t <m0t@users.noreply.github.com>
2014-09-19 18:24:43 +0100

    fix

--- decision_rules.okular.py
+++ decision_rules.okular.py
@@ -1,5 +1,5 @@
 #######DECISION RULES
-def get_weight(mean, sigma2, timecounter, cur_votes=0, save_arg=False):
+def get_weight(mean, sigma2, timecounter, cur_votes=0, quorum=1, save_arg=False):
     weight=0
     #give it a second will you
     if timecounter > 3:
@@ -12,14 +12,15 @@ def get_weight(mean, sigma2, timecounter, cur_votes=0, save_arg=False):
             weight -= 0.2
         if sigma2 == 0:
             weight += 0.2
-            if mean > 40 and self.votes >= self.quorum and self.save_arg==True:
-                self.save_votes=1
+            if mean > 40 and cur_votes >= quorum and save_arg==True:
+                #XXX save_votes needs fix
+                weight=1
         elif sigma2 <= 100 and mean <= 40:
             weight += 0.1
         elif sigma2 <= 100 and mean > 40:
             weight += 0.05
-            if self.votes >= self.quorum and self.save_arg==True:
-                self.save_votes=1
+            if cur_votes >= quorum and save_arg==True:
+                weight=1
         elif sigma2 > 200:
             weight -= 0.2
     
